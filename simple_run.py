04fb1e4d30eecef811705a65f3464564639e76aa
selman.ozleyen <syozleyen@gmail.com>
2025-05-15 12:25:33 +0200

    update script and make it a project

--- simple_run.py
+++ simple_run.py
@@ -1,21 +1,21 @@
-import os
 import time
 from benchmarks.project.loading import read_lazy, ZarrDataset
 from torch.utils.data import DataLoader
 import numpy as np
 import argparse
-
-DATA_PATH = os.path.expanduser("~/projects/dataloading-benchmark/benchmarks/data/synthetic_data.zarr")
+from pathlib import Path
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--data-path", type=str)
     parser.add_argument("--torch", action="store_true")
     parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--num_workers", type=int, default=4)
     args = parser.parse_args()
-
-    adata = read_lazy(DATA_PATH)
+    data_path = Path(args.data_path)
+    assert data_path.exists(), f"Data path {data_path} does not exist"
+    adata = read_lazy(data_path)
     loader = ZarrDataset(adata, "cell_type")
     if args.torch:
         loader = DataLoader(
@@ -30,11 +30,11 @@ if __name__ == "__main__":
     batch_sizes = []
     for x in loader:
         batch_times.append(time.time() - time0)
-        batch_sizes.append(1 if not args.torch else len(x[0]))
+        batch_sizes.append(1 if not args.torch else len(x[0])) # also to make sure it is not optimized out
         time0 = time.time()
     # give in plus and minus
-    print(f"Average batch size: {sum(batch_sizes)/len(batch_sizes)}")
     avg_batch_size = sum(batch_sizes)/(len(batch_sizes) * 1.0)
+    print(f"Average batch size: {avg_batch_size}")
     print(f"Average batch time: {sum(batch_times)/len(batch_times)} seconds (+/- {np.std(batch_times)} seconds)")
     print(f"Average time per element: {sum(batch_times)/(len(batch_times) * avg_batch_size)} seconds")
     print(f"Total time: {sum(batch_times)} seconds")
